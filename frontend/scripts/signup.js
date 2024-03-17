1fa568dd644fc3ff98f8fa0a335a2d3f35a3e000
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-18 00:59:30 +0200

    linked signup to the database

---
+++ frontend/scripts/signup.js
@@ -0,0 +1,59 @@
+const registerBtn = document.getElementById("register");
+const errorMessage = document.getElementById("error-message");
+const emailRegex = /^[^s@]+@[^s@]+.[^s@]+$/;
+
+function addUser() {
+  try {
+    const fullName = document.getElementById("full-name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const formData = new FormData();
+
+    formData.append("email", email);
+    formData.append("name", fullName);
+    formData.append("password", password);
+    formData.append("gender", gender);
+
+    fetch("http://127.0.0.1/Flight-System-Website/backend/signup.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Response from server:", data);
+
+        if (data.status === "error") {
+          errorMessage.textContent = data.message;
+        } else {
+          //   window.location.href = "/HTML/index.html";
+          console.log("You signed in");
+        }
+      });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+registerBtn.addEventListener("click", () => {
+  const fullName = document.getElementById("full-name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  if (password !== confirmPassword) {
+    errorMessage.textContent = "Passwords do not match. Try again.";
+  } else if (email === "" || fullName === "") {
+    errorMessage.textContent = "Inputs can't be empty";
+  } else if (!emailRegex.test(email)) {
+    errorMessage.textContent = "Invalid email address";
+  } else {
+    addUser();
+  }
+});
