4c907835e1633deacc439f6133bd733d90692451
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-19 20:53:43 +0200

    Finished profile page

--- frontend/Scripts/request_coin.js
+++ frontend/Scripts/request_coin.js
@@ -1,28 +1,31 @@
 const form = document.getElementById("requestCoinsForm");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const userID = document.getElementById("UserID").value;
-    const coinsAmount = document.getElementById("coinsAmount").value;
+  const userID = document.getElementById("UserID").value;
+  const coinsAmount = document.getElementById("coinsAmount").value;
 
-    const formData = new FormData();
-    formData.append("UserID", userID);
-    formData.append("coinsAmount", coinsAmount);
+  const formData = new FormData();
+  formData.append("UserID", userID);
+  formData.append("coinsAmount", coinsAmount);
 
-    axios
-        .post("http://localhost/Flight-System-Website/backend/request_coin.php", formData)
-        .then((response) => {
-            const data = response.data;
-            if (data.success) {
-                alert("Request sent successfully!");
-                form.reset();
-            } else {
-                alert("Error: " + data.message);
-            }
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-            alert("Error submitting review. Please try again.");
-        });
+  axios
+    .post(
+      "http://localhost/Flight-System-Website/backend/request_coin.php",
+      formData
+    )
+    .then((response) => {
+      const data = response.data;
+      if (data.success) {
+        alert("Request sent successfully!");
+        form.reset();
+      } else {
+        alert("Error: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Error submitting review. Please try again.");
+    });
 });
